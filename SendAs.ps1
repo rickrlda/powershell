9a81a67fb907de78e7a166ad96cb4429a2cf52d3
Ricardo Azevedo <ricardo_azevedo@outlook.com>
2019-03-11 16:51:30 -0300

    Update SendAs.ps1

--- SendAs.ps1
+++ SendAs.ps1
@@ -1,16 +1,16 @@
-﻿<#
-.SYNOPSIS
-    Adiciona Permissão de SendAs
-
-
-    .DESCRIPTION
-    Este script tem com finalidade adicionar permissão de SendAs na caixa de correio compartilhada.
-    Script desenvolvido para uso exclusivo do Tribunal de Justiça de SP.
-    
-    
-    Version 1.0, 25 de Fevereiro, 2019
-    Autor: Ricardo Azevedo
-
+#<#
+#.SYNOPSIS
+#    Adiciona Permissão de SendAs
+#
+#
+#    .DESCRIPTION
+#    Este script tem com finalidade adicionar permissão de SendAs na caixa de correio compartilhada.
+#    Script desenvolvido para uso exclusivo do Tribunal de Justiça de SP.
+#    
+#    
+#    Version 1.0, 25 de Fevereiro, 2019
+#    Autor: Ricardo Azevedo
+#
 #>
 
 [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')

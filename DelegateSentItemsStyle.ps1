8221aea051774bc0adbccde85d4bbd2f03e0c3ac
Ricardo Azevedo <ricardo_azevedo@outlook.com>
2019-03-14 17:40:34 -0300

    Update DelegateSentItemsStyle.ps1

--- DelegateSentItemsStyle.ps1
+++ DelegateSentItemsStyle.ps1
@@ -14,10 +14,22 @@
 
 #>
 
-[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
-$OfficeVersion = [Microsoft.VisualBasic.Interaction]::InputBox("Type the version of Office installed (16.0 = Office 2016, 15.0 = Office 2013, 14.0 = Office 2010)", "Office Version")
+$OfficeVersion = Read-Host ("Digite a versão do Microsoft Office instalada nesse equipamento. 
+Para a versão 2016 = 16.0
+Para a versão 2013 = 15.0
+Para a versão 2010 = 14.0
+Digite aqui a versão")
 $registryPath = "HKCU:\Software\Microsoft\Office\$OfficeVersion\Outlook\Preferences"
 $Name = "DelegateSentItemsStyle"
 $value = "1"
-New-ItemProperty -Path $registryPath -Name $name -Value $value `
+IF ((Test-Path $registryPath) -eq $true)
+    {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
     -PropertyType DWORD -Force | Out-Null
+    Write-Host "Chave de registro criada com sucesso!" -ForegroundColor green
+}
+ELSE {
+    Write-Host "P.S.:" -ForegroundColor yellow -BackgroundColor black -NoNewline
+    Write-Host (" Não foi possível localizar o caminho $registryPath porque ele não existe," +
+        " por favor verifique se a versão que foi informada do Microsoft Office corresponde com a versão instalada neste computador.") -ForegroundColor red -BackgroundColor black
+}

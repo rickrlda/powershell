8ec4c860f6a48b16fff6dcf620ac5be6dec2fc62
Ricardo Azevedo <ricardo_azevedo@outlook.com>
2019-03-14 17:41:46 -0300

    Update DelegateSentItemsStyle.ps1

--- DelegateSentItemsStyle.ps1
+++ DelegateSentItemsStyle.ps1
@@ -1,17 +1,19 @@
-﻿<#
-.SYNOPSIS
-    Adiciona chave de registro Outlook
-
-
-    .DESCRIPTION
-    Este script tem com finalidade adicionar a chave de regsitro DelegateSentItemsStyle para usuários que tem permissão de SnedAs e 
-    utilizam caixa compartilhada e enviam mensagem atraves dela.
-    Script desenvolvido para uso exclusivo do Tribunal de Justiça de SP.
-    
-    
-    Version 1.0, 11 de Março, 2019
-    Autor: Ricardo Azevedo
-
+#<#
+#.SYNOPSIS
+#    Adiciona chave de registro Outlook
+#
+#
+#    .DESCRIPTION
+#    Este script tem com finalidade adicionar a chave de regsitro DelegateSentItemsStyle para usuários que tem permissão de SnedAs e 
+#    utilizam caixa compartilhada e enviam mensagem atraves dela.
+#    Script desenvolvido para uso exclusivo do Tribunal de Justiça de SP.
+#    
+#    
+#    Version Created: 1.0, 11 de Março, 2019
+#    Version Updated (IF and ELSEIF): 1.1, 14 de Março, 2019
+#    Autor: Ricardo Azevedo
+#    KB Microsoft: https://support.microsoft.com/en-us/help/2843677/messages-sent-from-a-shared-mailbox-aren-t-saved-to-the-sent-items-fol
+#
 #>
 
 $OfficeVersion = Read-Host ("Digite a versão do Microsoft Office instalada nesse equipamento. 
@@ -26,7 +28,7 @@ IF ((Test-Path $registryPath) -eq $true)
     {
     New-ItemProperty -Path $registryPath -Name $name -Value $value `
     -PropertyType DWORD -Force | Out-Null
-    Write-Host "Chave de registro criada com sucesso!" -ForegroundColor green
+    Write-Host "Chave de registro criada com sucesso!!!" -ForegroundColor green
 }
 ELSE {
     Write-Host "P.S.:" -ForegroundColor yellow -BackgroundColor black -NoNewline

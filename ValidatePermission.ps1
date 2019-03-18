0d0f6a6d27b9c5e867a78ec4264eed3cc262b11e
Ricardo Azevedo <ricardo_azevedo@outlook.com>
2019-03-18 12:38:35 -0300

    Add files via upload

---
+++ ValidatePermission.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+    Verifica se o usuário possui permissão FullAcces & SendAs, caso negativo adiciona.
+
+
+    .DESCRIPTION
+    Este script tem como finalidade verificar se o usuário tem permissão na caixa de correio compartilhada, 
+    caso negativo ele adiciona a permissão de FullAccess e SendAs 
+    para os usuários que utilizam a caixa compartilhada.
+    Script desenvolvido para uso exclusivo do Tribunal de Justiça de SP.
+    
+    
+    Version 1.0, 18 de Março, 2019
+    Autor: Ricardo Azevedo
+
+#>
+
+$Identity = Read-Host "Digite o alias, email ou Displayname da Caixa Compartilhada"
+$User = Read-Host "Digite o alias, email ou DisplayName do Usuário"
+
+if ((Get-MailboxPermission -Identity $identity -User $User) -eq $null){
+    
+   Add-MailboxPermission -Identity $identity -User $User -AccessRights FullAccess -AutoMapping $false | Out-Null
+   Add-RecipientPermission -Identity $identity -Trustee $User -AccessRights SendAs -Confirm:$false | Out-Null
+   Write-Host "Permissão de FullAccess e SendAs adicionado para o usuário $User na Caixa Compartilhada: $identity" -ForegroundColor green
+}
+else {
+    Write-Host "O usuário $user já possui permissão na Caixa Compartilhada $identity" -ForegroundColor yellow
+}

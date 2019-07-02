1eb3ae09054e12dc0f97b50b74b0f1ede324adbd
Ricardo Azevedo <ricardo_azevedo@outlook.com>
2019-07-02 09:50:12 -0300

    Add files via upload

---
+++ RemoveSmtp.ps1
@@ -0,0 +1,13 @@
+﻿
+
+$Mailboxes = Import-Csv -Path C:\TEMP\EmailAddress.csv
+$grupoccr = "@grupoccr.intranet"
+$smtp = "smtp:$($Mailboxes.Alias)$grupoccr"
+
+
+foreach($Object in $Mailboxes){
+
+   Set-Mailbox -Identity $Mailboxes.Alias -EmailAddressPolicyEnabled $false | Out-Null
+   Set-Mailbox -Identity $Mailboxes.Alias -EmailAddresses @{remove=$smtp} | Out-Null
+   Write-Host "Desablitado EmailAddressPolicyEnabled do usuário $($Object.Alias) e removido o smtp $($Object.Alias)$grupoccr"
+}
